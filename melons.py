2346e53759090e7fab639bc9408c8c26bea7c0ad
jessicalynn1 <jessicalynnfaylor@gmail.com>
2021-11-07 00:31:38 +0000

    Mostly Finished Part 3

--- melons.py
+++ melons.py
@@ -2,10 +2,10 @@
 
 class AbstractMelonOrder():
 
-    def __init__(self, species, qty, shipped, order_type, tax, country_code):
+    def __init__(self, species, qty, order_type, tax, country_code):
         self.species = species
         self.qty = qty
-        self.shipped = shipped
+        self.shipped = False
         self.order_type = order_type
         self.tax = tax
         self.country_code = country_code
@@ -21,10 +21,9 @@ class AbstractMelonOrder():
 
         return total
     
-    #def mark_shipped_code(self):
-    #     """Return the country code."""
-
-    #return self.country_code
+    def mark_shipped(self):
+        """Return true when shipped."""        
+        self.shipped = True
 
 class DomesticMelonOrder(AbstractMelonOrder):
     """A melon order within the USA."""
@@ -34,20 +33,14 @@ class InternationalMelonOrder(AbstractMelonOrder):
     """An international (non-US) melon order."""
     tax = 0.17
     
+class GovernmentMelonOrder(AbstractMelonOrder):
+    '''No tax on gov't orders'''
+    # tax = 0.0    
+    #passed_inspection = 
+    def __init__(self):
+        super().__init__(species, qty, order_type="Government", tax=0.0)
+
+        self.passed_inspection = False
     
-    #get_total.total = total + 3 
-    # def xmas_total(self):
-    #     if self.qty < 10:
-    #         super().get_total()
-
-    # def get_total(self):
-    #     """Calculate price, including tax."""
-    #     if self.qty < 10:
-    #         total = total + 3
-
-    #     return total
-
-    # def get_country_code(self):
-    #     """Return the country code."""
-
-    #     return self.country_code
+    def mark_inspection(self,passed_inspection):
+        self.passed_inspection = True
